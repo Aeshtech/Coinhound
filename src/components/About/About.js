1d948fb23d2129aae9730549b131dca6705263e0
aeshtech <dev.aeshtech@gmail.com>
2022-12-09 17:03:56 +0530

    changes in about image

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,21 +5,21 @@ export default function About() {
         <>
             <div className="aboutImagesContainer">
                 <div class="mousemove">
-                    <div class="dh_1 posab c1 mouse" value="2"><img src="./img/about/1.png" className="img-responsive" alt="" /></div>
-                    <div class="dh_2 posab c2 mouse" value="3"><img src="./img/about/2.png" className="img-responsive" alt="" /></div>
-                    <div class="dh_2 posab c3 mouse" value="4"><img src="./img/about/3.png" className="img-responsive" alt="" /></div>
-                    <div class="dh_2 posab c4 mouse" value="3"><img src="./img/about/4.png" className="img-responsive" alt="" /></div>
-                    <div class="dh_2 posab c5 mouse" value="5"><img src="./img/about/5.png" className="img-responsive" alt="" /></div>
-                    <div class="dh_2 posab c6 mouse" value="3"><img src="./img/about/6.png" className="img-responsive" alt="" /></div>
-                    <div class="dh_2 posab c7 mouse" value="3"><img src="./img/about/7.png" className="img-responsive" alt="" /></div>
-                    <div class="dh_2 posab c8 mouse" value="4"><img src="./img/about/8.png" className="img-responsive" alt="" /></div>
-                    <div class="dh_2 posab c9 mouse" value="3"><img src="./img/about/9.png" className="img-responsive" alt="" /></div>
-                    <div class="dh_2 posab c10 mouse" value="3"><img src="./img/about/10.png" className="img-responsive" alt="" /></div>
-                    <div class="dh_2 posab c11 mouse" value="5"><img src="./img/about/11.png" className="img-responsive" alt="" /></div>
-                    <div class="dh_2 posab c12 mouse" value="3"><img src="./img/about/12.png" className="img-responsive" alt="" /></div>
-                    <div class="dh_2 posab c13 mouse" value="3"><img src="./img/about/13.png" className="img-responsive" alt="" /></div>
-                    <div class="dh_2 posab c14 mouse" value="6"><img src="./img/about/14.png" className="img-responsive" alt="" /></div>
-                    <div class="dh_2 posab c15 mouse" value="3"><img src="./img/about/15.png" className="img-responsive" alt="" /></div>
+                    <div class="posab c1 mouse" value="2"><img src="./img/about/1.png" className="img-responsive" alt="" /></div>
+                    <div class="posab c2 mouse" value="3"><img src="./img/about/2.png" className="img-responsive" alt="" /></div>
+                    <div class="posab c3 mouse" value="4"><img src="./img/about/3.png" className="img-responsive" alt="" /></div>
+                    <div class="posab c4 mouse" value="3"><img src="./img/about/4.png" className="img-responsive" alt="" /></div>
+                    <div class="posab c5 mouse" value="5"><img src="./img/about/5.png" className="img-responsive" alt="" /></div>
+                    <div class="posab c6 mouse" value="3"><img src="./img/about/6.png" className="img-responsive" alt="" /></div>
+                    <div class="posab c7 mouse" value="3"><img src="./img/about/7.png" className="img-responsive" alt="" /></div>
+                    <div class="posab c8 mouse" value="4"><img src="./img/about/8.png" className="img-responsive" alt="" /></div>
+                    <div class="posab c9 mouse" value="3"><img src="./img/about/9.png" className="img-responsive" alt="" /></div>
+                    <div class="posab c10 mouse" value="3"><img src="./img/about/10.png" className="img-responsive" alt="" /></div>
+                    <div class="posab c11 mouse" value="5"><img src="./img/about/11.png" className="img-responsive" alt="" /></div>
+                    <div class="posab c12 mouse" value="3"><img src="./img/about/12.png" className="img-responsive" alt="" /></div>
+                    <div class="posab c13 mouse" value="3"><img src="./img/about/13.png" className="img-responsive" alt="" /></div>
+                    <div class="posab c14 mouse" value="6"><img src="./img/about/14.png" className="img-responsive" alt="" /></div>
+                    <div class="posab c15 mouse" value="3"><img src="./img/about/15.png" className="img-responsive" alt="" /></div>
                 </div>
                 <section className="aboutSection text-center d-flex align-items-center">
                     <div className="glow"></div>
