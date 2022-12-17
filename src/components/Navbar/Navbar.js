bf66e8829aa93cc8543063124626cd4e4eb3abcc
aeshtech <dev.aeshtech@gmail.com>
2022-12-17 14:30:04 +0530

    styled sidenav items

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import Context from '../../Contexts/Context';
 
 
@@ -14,16 +14,12 @@ export default function Navbar() {
         sideNavbar?.addEventListener('mouseleave', () => mainView.style.paddingLeft = "90px");
     }
 
-    // Add active class to the current button (highlight it)
-var header = document.getElementById("sideNavbar");
-var btns = header.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  this.className += " active";
-  });
-}
+    const location = useLocation();
+    const dashboard = location.pathname === "/dashboard" ? "active" : "";
+    const tracking = location.pathname === "/tracking" ? "active" : "";
+    const preScanWallet = location.pathname === "/preScanWallet" ? "active" : "";
+    const scanToken = location.pathname === "/scanToken" ? "active" : "";
+    const trendAnalysis = location.pathname === "/trendAnalysis" ? "active" : "";
 
     return (
         <>
@@ -42,110 +38,110 @@ for (var i = 0; i < btns.length; i++) {
                         </div>
                     </div>
                 </nav>
-                    <div className='sideNavbar overflow-y-auto' id='sideNavbar' onMouseOver={() => { resizeMainView() }}>
-                        <ul className="">
-                            <div className='overflow-auto' style={{ maxHeight: "92%" }}>
-                                {isWalletConnected ?
-                                    <span className='navCategory c1'>MY COINHOUND</span>
-                                    : null
-                                }
-                                <li className={isWalletConnected ? "l1 wConnected active" : "l1"}>
-                                    <Link to={"/dashboard"}>
-                                        {isWalletConnected ?
-                                            <img src="./img/icons/Clock.svg" alt="" className='mw-100 icon1' />
-                                            :
-                                            <img src="./img/icons/Wallet.svg" alt="" className='mw-100 icon1' />
-                                        }
-                                        <span className='nav-item'>Dashboard</span>
-                                    </Link>
-                                </li>
-                                {isWalletConnected ?
-                                    <>
-                                        <li className='l1'>
-                                            <Link to={"/dashboard"}>
-                                                <img src="./img/icons/Tracking.svg" alt="" className='mw-100 icon1' />
-                                                <span className='nav-item'>Tracking</span>
-                                            </Link>
-                                        </li>
-                                        <span className='navCategory c2'>TOOLS</span>
-                                    </>
-                                    : null
-                                }
-                                <li className='l2'>
-                                    <Link to={"/preScanWallet"}>
-                                        {isWalletConnected ?
-                                            <img src="./img/icons/ScanWallet2.svg" alt="" className='mw-100 icon2' />
-                                            :
-                                            <img src="./img/icons/wallet.svg" alt="" className='mw-100 icon2' />
-                                        }
-                                        <span className='nav-item'>Scan Wallet</span>
-                                    </Link>
-                                </li>
-                                <li className='l3'>
-                                    <Link to={"/scanToken"}>
-                                        {isWalletConnected ?
-                                            <img src="./img/icons/ScanToken2.svg" alt="" className='mw-100 icon3' />
-                                            :
-                                            <img src="./img/icons/dollar.svg" alt="" className='mw-100 icon3' />
-                                        }
-                                        <span className='nav-item'>Scan Token</span>
-                                    </Link>
-                                </li>
-                                <li className='l4'>
-                                    <Link to={"/trendAnalysis"}>
-                                        {isWalletConnected ?
-                                            <img src="./img/icons/Trending.svg" alt="" className='mw-100 icon4' />
-                                            :
-                                            <img src="./img/icons/vector2.svg" alt="" className='mw-100 icon4' />
-                                        }
-                                        <span className='nav-item'>Trend Analysis</span>
-                                    </Link>
-                                </li>
-                                <li className='l5'>
-                                    <Link>
-                                        {isWalletConnected ?
-                                            <img src="./img/icons/ScanBlockchain.svg" alt="" className='mw-100 icon5' />
-                                            :
-                                            <img src="./img/icons/dollar.svg" alt="" className='mw-100 icon5' />
-                                        }
-                                        <span className='nav-item'>Scan Blockchain</span>
-                                    </Link>
-                                </li>
-                                {isWalletConnected ?
-                                    <span className='navCategory c3'>DOG HOUSE</span>
-                                    : null
-                                }
-                                {isWalletConnected ?
-                                    <li className="l6">
-                                        <Link>
-                                            <img src="./img/icons/Bone.svg" alt="" className='mw-100 icon6' />
-                                            <span className='nav-item'>Stake CND</span>
-                                        </Link>
-                                    </li>
-                                    :
-                                    <li className='l6 mt-5'>
-                                        <Link>
-                                            <img src="./img/icons/dog.svg" alt="" className='mw-100 icon6' />
-                                            <span className='nav-item'>Treats</span>
-                                        </Link>
-                                    </li>
-                                }
-                                <li className='l7'>
-                                    <Link>
-                                        <img src="./img/icons/vector1.svg" alt="" className='mw-100 icon7' />
-                                        <span className='nav-item'> {isWalletConnected ? "Buy Collar" : "Collar"}</span>
-                                    </Link>
-                                </li>
-                            </div>
-                            <li className='l8'>
-                                <Link>
-                                    <img src="./img/icons/logout.svg" alt="" className='mw-100 icon8' />
-                                    <span className='nav-item'>Logout</span>
+                <div className='sideNavbar overflow-y-auto' id='sideNavbar' onMouseOver={() => { resizeMainView() }}>
+                    <ul className="">
+                        <div className='overflow-auto' style={{ maxHeight: "92%" }}>
+                            {isWalletConnected ?
+                                <span className='navCategory c1'>MY COINHOUND</span>
+                                : null
+                            }
+                            <li className={isWalletConnected ? `l1 wConnected ${dashboard}` : `l1 ${dashboard}`}>
+                                <Link to={"/dashboard"}>
+                                    {isWalletConnected ?
+                                        <img src="./img/icons/Clock.svg" alt="" className='mw-100 icon1' />
+                                        :
+                                        <img src="./img/icons/Wallet.svg" alt="" className='mw-100 icon1' />
+                                    }
+                                    <span className='nav-item'>Dashboard</span>
                                 </Link>
                             </li>
-                        </ul>
-                    </div>
+                            {isWalletConnected ?
+                                <>
+                                    <li className={`li ${tracking}`}>
+                                        <Link to={"/dashboard"}>
+                                            <img src="./img/icons/Tracking.svg" alt="" className='mw-100 icon1' />
+                                            <span className='nav-item'>Tracking</span>
+                                        </Link>
+                                    </li>
+                                    <span className='navCategory c2'>TOOLS</span>
+                                </>
+                                : null
+                            }
+                            <li className={`l2 ${preScanWallet}`}>
+                                <Link to={"/preScanWallet"}>
+                                    {isWalletConnected ?
+                                        <img src="./img/icons/ScanWallet2.svg" alt="" className='mw-100 icon2' />
+                                        :
+                                        <img src="./img/icons/wallet.svg" alt="" className='mw-100 icon2' />
+                                    }
+                                    <span className='nav-item'>Scan Wallet</span>
+                                </Link>
+                            </li>
+                            <li className={`l3 ${scanToken}`}>
+                                <Link to={"/scanToken"}>
+                                    {isWalletConnected ?
+                                        <img src="./img/icons/ScanToken2.svg" alt="" className='mw-100 icon3' />
+                                        :
+                                        <img src="./img/icons/dollar.svg" alt="" className='mw-100 icon3' />
+                                    }
+                                    <span className='nav-item'>Scan Token</span>
+                                </Link>
+                            </li>
+                            <li className={`l4 ${trendAnalysis}`}>
+                                <Link to={"/trendAnalysis"}>
+                                    {isWalletConnected ?
+                                        <img src="./img/icons/Trending.svg" alt="" className='mw-100 icon4' />
+                                        :
+                                        <img src="./img/icons/vector2.svg" alt="" className='mw-100 icon4' />
+                                    }
+                                    <span className='nav-item'>Trend Analysis</span>
+                                </Link>
+                            </li>
+                            <li className='l5'>
+                                <Link>
+                                    {isWalletConnected ?
+                                        <img src="./img/icons/ScanBlockchain.svg" alt="" className='mw-100 icon5' />
+                                        :
+                                        <img src="./img/icons/dollar.svg" alt="" className='mw-100 icon5' />
+                                    }
+                                    <span className='nav-item'>Scan Blockchain</span>
+                                </Link>
+                            </li>
+                            {isWalletConnected ?
+                                <span className='navCategory c3'>DOG HOUSE</span>
+                                : null
+                            }
+                            {isWalletConnected ?
+                                <li className="l6">
+                                    <Link>
+                                        <img src="./img/icons/Bone.svg" alt="" className='mw-100 icon6' />
+                                        <span className='nav-item'>Stake CND</span>
+                                    </Link>
+                                </li>
+                                :
+                                <li className='l6 mt-5'>
+                                    <Link>
+                                        <img src="./img/icons/dog.svg" alt="" className='mw-100 icon6' />
+                                        <span className='nav-item'>Treats</span>
+                                    </Link>
+                                </li>
+                            }
+                            <li className='l7'>
+                                <Link>
+                                    <img src="./img/icons/vector1.svg" alt="" className='mw-100 icon7' />
+                                    <span className='nav-item'> {isWalletConnected ? "Buy Collar" : "Collar"}</span>
+                                </Link>
+                            </li>
+                        </div>
+                        <li className='l8'>
+                            <Link>
+                                <img src="./img/icons/logout.svg" alt="" className='mw-100 icon8' />
+                                <span className='nav-item'>Logout</span>
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
+            </div>
         </>
 
     )
